63a086f06e87795b329eb4ec897e2703d829d727
yooodleee <accia25@naver.com>
2025-12-05 16:01:31 +0900

    variable argument macro

---
+++ Extreme-C/essential/macro/UsingAVariableArgumentMacroThatMimicsLoop.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+#define LOOP_5(X) printf("%s\n", #X)
+#define LOOP_4(X, ...) printf("%s\n", #X); LOOP_5(__VA_ARGS__)
+#define LOOP_3(X, ...) printf("%s\n", #X); LOOP_4(__VA_ARGS__)
+#define LOOP_2(X, ...) printf("%s\n", #X); LOOP_3(__VA_ARGS__)
+#define LOOP_1(X, ...) printf("%s\n", #X); LOOP_2(__VA_ARGS__)
+
+#define LOOP(...) LOOP_1(__VA_ARGS__)
+
+int main() {
+
+    LOOP(copy, paste, cut);
+    LOOP(copy, paste, cut, select);
+
+    return 0;
+}
